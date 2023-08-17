8becc4ecea412a3ae7d114eff17a4eaffecd92e8
xxai.art <xxai.art@gmail.com>
2023-08-17 22:40:49 +0800

    ♨

--- tests/main.rs
+++ tests/main.rs
@@ -1,7 +1,6 @@
 use lazy_static::lazy_static;
 use pgw::{Pg, Sql};
 use tokio::time;
-use tokio_postgres::types::Oid;
 
 lazy_static! {
   // get postgres connection uri from environment ( without prefix )
@@ -9,10 +8,26 @@ lazy_static! {
   // prepared sql
   static ref SQL_NSPNAME: Sql = PG.sql("SELECT oid FROM pg_catalog.pg_namespace LIMIT 2");
 }
+use tokio_postgres::types::Oid;
+//
+// lazy_static! {
+// pub static ref SQL_LI: pgw::Sql = PG.sql("SELECT task.id FROM bot.task,bot.civitai_img WHERE hash IS NOT NULL AND bot.task.rid=bot.civitai_img.id AND task.adult=0 AND cid=1 ORDER BY star DESC LIMIT 512");
+// }
+//
+// pub async fn li() -> Result<Vec<u64>, pgw::Error> {
+//   Ok(
+//     PG.query(&*SQL_LI, &[])
+//       .await?
+//       .iter()
+//       .map(|r| r.get::<_, u64>(0))
+//       .collect(),
+//   )
+// }
 
 #[tokio::test]
 async fn main() -> anyhow::Result<()> {
   loginit::init();
+  // dbg!(li().await?);
   for i in 0..99999 {
     println!("loop {i}");
     match PG.query(&*SQL_NSPNAME, &[]).await {
